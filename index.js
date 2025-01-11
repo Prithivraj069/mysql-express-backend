f643e0efce2a69b22e396b617485a2b0b0e788ef
Prithivraj Saravanan <prithivisaravanan069@gmail.com>
2025-01-11 06:56:46 +0000

    show customer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const wax = require('wax-on');
 require('dotenv').config();
+const {createConnection} = require('mysql2/promise');
 
 let app = express();
 app.set('view engine', 'hbs');
@@ -12,13 +13,57 @@ wax.on(hbs.handlebars);
 wax.setLayoutPath('./views/layouts');
 
 const helpers = require('handlebars-helpers');
+// const { createConnection } = require('mysql2');
 helpers({
     'handlebars':hbs.handlebars
 })
 
-app.get('/',(req, res)=> {
-res.send("Hello welcome to backend development");
-})
+
+
+async function main() {
+   const connection = await createConnection({
+        'host': process.env.DB_HOST,
+        'user': process.env.DB_USER,
+        'database': process.env.DB_NAME,
+        'password': process.env.DB_PASSWORD
+    });
+
+    app.get('/',async function (req, res) {
+        res.render('index')
+    })
+
+    app.get('/customers', async function (req, res) {
+
+        let query = `SELECT * FROM customers JOIN
+        companies ON companies.company_id = customers.company_id WHERE 1`
+
+        const bind = [];
+        const {first_name, last_name} = req.query;
+
+        if(first_name) {
+            query += `AND first_name LIKE ?`;
+            bind.push('%' + first_name + '%');
+        }
+
+        if(last_name) {
+            query += `AND last_name LIKE ?`;
+            bind.push('%' + last_name + '%');
+        }
+        console.log(query);
+
+        const [customers] = await connection.execute({
+            'sql': query,
+            'nestTables': true
+        }, bind);
+
+        res.render('customers.hbs', {
+            "allCustomers": customers,
+            "searchTerms":req.query
+        })
+    })
+}
+
+main();
 
 app.listen(3000, ()=>{
     console.log("server start");
